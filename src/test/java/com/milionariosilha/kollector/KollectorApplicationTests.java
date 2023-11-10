ed7e9302192c8132175c8397cd4f12254ceb9c83
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-11-10 15:09:12 -0300

    :ok_hand: style: checkstyle config

--- src/test/java/com/milionariosilha/kollector/KollectorApplicationTests.java
+++ src/test/java/com/milionariosilha/kollector/KollectorApplicationTests.java
@@ -1,13 +1,8 @@
 package com.milionariosilha.kollector;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KollectorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
